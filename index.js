229f79ed4874db701e67bc9640e4c19c592b2122
LaPhuocLoc <tsusura173@gmail.com>
2022-01-14 09:51:55 +0900

    a

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const app = {
         {
             name: "たぶん",
             singer: "Yoasobi",
-            path: "./Yoasobi/audio/Tabun.mp3",
+            path: "./Yoasobi/audio/Tabun.m4a",
             image: "./Yoasobi/img/Tabun.jpg"
         },
         {
